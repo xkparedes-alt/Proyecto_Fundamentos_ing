b257e95790c5310ad9fce78c63a83da2a422b3c2
kevin3038153 <kevinalex1119@gmail.com>
2026-01-16 11:49:27 -0500

    upload class atacante.py

---
+++ atacante.py
@@ -0,0 +1,11 @@
+class Atacante:
+    def _init_(self):
+        self.nombre = "Guerrero de Sombras"
+        self.vida = 80
+        self.ataque = 90
+        self.energia = 40
+
+    def accion_especial(self):
+        return "¡GOLPE CRÍTICO! Has causado 500 de daño masivo al enemigo."
+
+heroe_atacante = Atacante()
